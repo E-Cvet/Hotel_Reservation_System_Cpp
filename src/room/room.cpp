10a4805f82066ef78724b469b75b4218b99edad9
Kiril <kirildimovski2006@gmail.com>
2026-05-20 22:30:16 +0200

    added codes for rooms

--- src/room/room.cpp
+++ src/room/room.cpp
@@ -6,3 +6,83 @@ string Room::get_room_number()
     return this->room_number;
 }
 
+#include "room.h"
+
+// room klasa
+
+Room::Room(int number, double price)
+    : roomNumber(number), pricePerNight(price), available(true) {}
+
+int Room::getRoomNumber() const {
+    return roomNumber;
+}
+
+double Room::getPrice() const {
+    return pricePerNight;
+}
+
+bool Room::isAvailable() const {
+    return available;
+}
+
+void Room::setAvailability(bool status) {
+    available = status;
+}
+
+// 1 room+
+
+SingleRoom::SingleRoom(int number, double price)
+    : Room(number, price) {}
+
+void SingleRoom::display_info() const {
+    cout << "Single Room | Number: " << roomNumber
+         << " | Price: " << pricePerNight
+         << " | Available: " << (available ? "Yes" : "No")
+         << endl;
+}
+
+// 2 room
+
+DoubleRoom::DoubleRoom(int number, double price)
+    : Room(number, price) {}
+
+void DoubleRoom::display_info() const {
+    cout << "Double Room | Number: " << roomNumber
+         << " | Price: " << pricePerNight
+         << " | Available: " << (available ? "Yes" : "No")
+         << endl;
+}
+
+// suite
+
+Suite::Suite(int number, double price)
+    : Room(number, price) {}
+
+void Suite::display_info() const {
+    cout << "Suite | Number: " << roomNumber
+         << " | Price: " << pricePerNight
+         << " | Available: " << (available ? "Yes" : "No")
+         << endl;
+}
+
+// Room manager
+
+void RoomManager::addRoom(shared_ptr<Room> room) {
+    rooms.push_back(room);
+}
+
+shared_ptr<Room> RoomManager::findRoom(int roomNumber) {
+    for (auto room : rooms) {
+        if (room->getRoomNumber() == roomNumber) {
+            return room;
+        }
+    }
+
+    return nullptr;
+}
+
+void RoomManager::listRooms() const {
+    for (const auto& room : rooms) {
+        room->display_info();
+    }
+}
--- src/room/room.h
+++ src/room/room.h
@@ -4,94 +4,72 @@
 
 using namespace std;
 
-/*
-    Info to whoever is in charge of the Rooms.
+#ifndef ROOM_H
+#define ROOM_H
 
-    The proected attributes of Rooms can be whatever you want. That is for you to decide. 
-    Please leave the public methods of rooms these as they are needed in the other functionalities.
+#include <iostream>
+#include <vector>
+#include <memory>
+#include <string>
 
-    Why the public functions are needed:
-        Room (string number, double price) - Constructor, this is how the object is created, every room should have
-                a room number by which it will be identified in the system and the price per night that is worth.
+using namespace std;
 
-        virtual void display_info() - Every child Room that you create should implement this function to display
-                information about that specific room.
-
-        double get_price() - The price of that type of room per night.
-
-        bool get_avaibalitiy() - If the room is available or not.
-
-        void set_availability() - Make it available or not.
-
-
-    Room Manager:
-        Same goes for here, the private attributes can be changed compltely
-
-    Please do not change the public attribute signatures, this is why they are needed:
-
-    This functioanlity will be used in the Reservation.
-    Room* find_room(string room_number) - Checks if a room is in the system
-
-    These functionalities are used in main:
-    void add_room(Room* room) - Adds a room in the system
-
-    void list_all_rooms() - prints all the rooms in the system 
-
-    void list_available_rooms() - prints only the available rooms in the system
-
-
-    Furthermore, go wild with any implenetation of any child classes.
-*/
-
-/* Abstract base class — all room types must have this. This implementation is concrete.*/
+// abstract klasa
 class Room {
 protected:
-    string room_number;
-    double price_per_night;
-    bool isAvailable;
+    int roomNumber;
+    double pricePerNight;
+    bool available;
 
 public:
-    Room(string number, double price);
+    Room(int number, double price);
 
-    virtual void display_info() = 0;
+    virtual ~Room() {}
 
-    string get_room_number();
-    double get_price();
-    bool get_availability();
-    void set_availability(bool status);
+    int getRoomNumber() const;
+    double getPrice() const;
+    bool isAvailable() const;
+
+    void setAvailability(bool status);
+
+    virtual void display_info() const = 0;
 };
 
-
-/* Examples for rooms that can be created. These are not concrete: */
+// 1 room
 class SingleRoom : public Room {
 public:
-    SingleRoom(int number);
-    void display_info() override;
+    SingleRoom(int number, double price);
+
+    void display_info() const override;
 };
 
+// 2 room
 class DoubleRoom : public Room {
 public:
-    DoubleRoom(int number);
-    void display_info() override;
+    DoubleRoom(int number, double price);
+
+    void display_info() const override;
 };
 
+// suite
 class Suite : public Room {
-    bool has_jacuzzi;  
-
 public:
-    Suite(int number, bool jacuzzi = false);
-    void display_info() override;
+    Suite(int number, double price);
+
+    void display_info() const override;
 };
 
-// Manages the collection of all rooms
+// room manager
 class RoomManager {
 private:
-    vector<Room*> rooms;  // owns the pointers
+    vector<shared_ptr<Room>> rooms;
 
 public:
-    void add_room(Room* room);
-    Room* find_room(string room_number);
-    void list_all_rooms();
-    void list_available_rooms();
-    ~RoomManager();  // cleans up Room* pointers
+    void addRoom(shared_ptr<Room> room);
+
+    shared_ptr<Room> findRoom(int roomNumber);
+
+    void listRooms() const;
 };
+
+#endif
